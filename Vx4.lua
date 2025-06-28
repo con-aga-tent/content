19273a37992f984f95ad26da4f4b38bd76133a68
MicrosoftExcelHub <delta.551616@gmail.com>
2025-06-28 11:38:25 +0400

    Update Vx4.lua

--- Vx4.lua
+++ Vx4.lua
@@ -1,12 +1,16 @@
-local repo = "https://raw.githubusercontent.com/MicrosoftExcelHub/Script-Games/refs/heads/main/"
+local repo = "https://raw.githubusercontent.com/MicrosoftExcelHub/Script-Games/main/Games/"
+
+-- Game-specific scripts table (PlaceId → Script URL)
 local Games = {
-    [127707120843339] =  "https://raw.githubusercontent.com/MicrosoftExcelHub/Script-Games/refs/heads/main/MathMurder.lua",
-    [12361295702] = "https://raw.githubusercontent.com/MicrosoftExcelHub/Script-Games/refs/heads/main/NCT.lua"
+    [127707120843339] = repo .. "MathMurder.lua",
+    [12361295702] = repo .. "NCT.lua"
 }
+
 local currentPlaceId = game.PlaceId
 local scriptUrl = Games[currentPlaceId]
+
 if scriptUrl then
     loadstring(game:HttpGet(scriptUrl))()
 else
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/MicrosoftExcelHub/Script-Games/refs/heads/main/Uni.lua"))()
+    loadstring(game:HttpGet(repo .. "Uni.lua"))()
 end
